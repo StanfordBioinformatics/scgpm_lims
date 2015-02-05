fa7c835bae10d259b99b43bef79610e38744b44d
nathankw <nathankw@stanford.ed>
2015-02-05 11:57:33 -0800

    Added getters to RunInfo class in models.py

--- components/models.py
+++ components/models.py
@@ -5,15 +5,21 @@ class RunInfo:
     RUN_SEQUENCING_FAILED = 0
 
     def __init__(self, conn, run):
-        self.obj = conn.getruninfo(run=run)
+        obj = conn.getruninfo(run=run)
+        self.ri = obj['run_info'] #self.ri = run info object
+        self.runId =obj['id'] 
 
-    def get_run_name(self):
-        return self.obj['run_info']['run_name']
+    def getRunName(self):
+        return self.ri['run_name']
       
-    def set_run_status(self, status):
+    def getLane(self,lane):
+        lane = str(lane)
+        return self.Lane(self.ri['lanes'][lane])
+
+    def setRunStatus(self, status):
         pass #TODO
 
-    def get_run_status(self, status):
+    def getRunStatus(self, status):
         pass #TODO
 
     def getpipelinerunid(self, run, lane=None, status='done'):
@@ -51,23 +57,78 @@ class RunInfo:
 
         return _getlatest(pipeline_runs, status)
 
-#    class Lane:
-#        emailReg = re.compile('\w{3,20}@\w{3,20}\.\w{3}')
-#        def __init__(self,laninfo):
-#            self.laneinfo = laneinfo
-#            self.notify_emails = [x['email'].strip() for x in self.laneinfo['notify']]
-#            notify_comments = None
-#            try:
-#                notify_comments = self.laneinfo['notify_comments']
-#            except KeyError:
-#                pass
-#            if notify_comments:
-#                potentialEmails = re.split(r'[ ,;]',notify_comments)
-#                potentialEmails = [x.strip() for x in potentialEmails]
-#                potentialEmails = emaiReg.findall(potentialEmails)
-#                if potentialEmails:
-#                    for i in potentialEmails:
-#                        self.notify_emails.append(i)
+    class LaneInfo:
+        emailReg = re.compile('\w{3,20}@\w{3,20}\.\w{3}')
+        def __init__(self,laninfo):
+            """
+            Args: laneinfo - A dict that is one of the values of the lanes in self.ri['lanes'].
+            """
+            self.li = laneinfo
+      
+        def __getitem__(self,key):
+            return self.li[key] 
+
+        def getDnaLibraryId(self):
+            return self['dna_library_id']
+
+        def getBarcodeSize1(self):
+            return self['barcode_size']
+
+        def getLab(self):
+            return self['lab']
+ 
+        def getMappingRequests(self):
+            return self['mapping_requests']
+
+        def getQueue(self):
+            return self['queue']
+ 
+        def getSubmitter(self):
+            return self['submitter']
+      
+        def getBarcodePosition(self):
+            return self['barcode_position']
+
+        def getSequencingRequest(self):
+            return self['sequencing_request']
+
+        def getNotify(self):
+            return self['notify']
+
+        def getSampleName(self):
+            return self['sample_name']
+
+        def getOwner(self):
+            return self['owner']
+   
+        def getBarcodeSize2(self):
+            return self['barcode_size2']
+        
+        def isMultiplexed(self):
+            return self['multiplexed']
+ 
+            self.notify_emails = [x['email'].strip() for x in self.li['notify']]
+            notify_comments = None
+            try:
+                notify_comments = self.li['notify_comments']
+            except KeyError:
+                pass
+            if notify_comments:
+                potentialEmails = re.split(r'[ ,;]',notify_comments)
+                potentialEmails = [x.strip() for x in potentialEmails]
+                potentialEmails = emaiReg.findall(potentialEmails)
+                if potentialEmails:
+                    for i in potentialEmails:
+                        self.notify_emails.append(i)
+
+        def getLaneId(self):
+            return self['id']
+
+        def getSubmitterEmail(self):
+            return self['submitter_email']
+
+        def getBarcodes(self):
+            return self['barcodes']
                         
 #    def __init__(self,runinfo):
 #        """
