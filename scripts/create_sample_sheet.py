eaaf783aaa48a0a03e0934b52e0c7a5fadf411c2
Nathaniel Watson <nathankw@stanford.edu>
2015-10-05 12:10:16 -0700

    Added defaults for LIMS credentials

--- scripts/create_sample_sheet.py
+++ scripts/create_sample_sheet.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from scgpm_lims import Connection
 import json
 import argparse
 
+try:
+	token = os.environ["UHTS_LIMS_TOKEN"]
+except KeyError:
+	token = None
+try:
+	url = os.environ["UHTS_LIMS_URL"]
+except KeyError:
+	url = None
+
 description = """
-Creates a sample sheet for demultiplexing. The sample sheet can be created for all lanes on the given run, or 
+Create a sample sheet for demultiplexing by communicating with UHTS. The sample sheet can be created for all lanes on the given run, or 
 just the specified lane. Supports bcl2fastq 1x and 2x. For 2x, the second index (I5) is reverse-complemented
 with respect to what's stored in UHTS. As stated in the Illumina docs: For Illumina sequencing systems running
 RTA version 1.18.54 and later, use bcl2fastq2 Conversion Software v2.17.  For Illumina sequencing systems runnings
@@ -18,8 +28,14 @@ The version of RTA used in the sequencing run can be found in the runParameters.
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,description=description)
 
 parser.add_argument('-r', '--run_name', help='Name of the run.', required=True)
-parser.add_argument('-t', '--lims_token', help='LIMS access token', required=True)
-parser.add_argument('-u', '--lims_url', help='LIMS url', required=True)
+if token:
+	parser.add_argument('-t', '--lims_token', help='LIMS access token. Default is set by UHTS_LIMS_TOKEN environment variable.', default = token)
+else:
+	parser.add_argument('-t', '--lims_token', help='LIMS access token.',required=True)
+if url:
+	parser.add_argument('-u', '--lims_url', help='LIMS url. Default is set by UHTS_LIMS_URL environment variable (%(default)s).', default = url)
+else:
+	parser.add_argument('-u', '--lims_url', help='LIMS url', required=True)
 parser.add_argument('-b','--bcl2fastq-version',required=True,type=int,help="int. The major version number of the bcl2fastq demultiplexer that was used to demultiplex the run.")
 parser.add_argument('-l','--lane',type=int,help="The number of the lane sequenced on the flowcell.")
 
